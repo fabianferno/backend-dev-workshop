0fb88ed7115dacf1b72caa2ad501215b8ec39493
fabianferno <fabianferno@gmail.com>
2022-02-09 02:15:02 +0530

    Add jwt verify

--- src/api-v1/index.ts
+++ src/api-v1/index.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 import users from "./users/users.route";
 import posts from "./posts/posts.route";
+// import authorize from "../helpers/jwtVerify";
 
 const router: Router = Router();
 
 router.use("/users", users);
 router.use("/posts", posts);
+
+// only for authorized users
+// router.use("/posts", authorize, posts);
 
 export default router;
